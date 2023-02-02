a615cd644dde94de97508c4bb55e604877b2a85a
gillohsylvia <abdallahfatima363@gmail.com>
2023-02-02 15:04:45 +0000

    revised

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# This Puppet script will replace a line in a file on a apache server
+# This Puppet script will replace a line in a file on a server
 
 $file_to_edit = '/var/www/html/wp-settings.php'
 
